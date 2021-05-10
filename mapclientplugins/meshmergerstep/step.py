70d380a21ff37c615ab78969b399acbe27dd4fc6
Richard Christie <r.christie@auckland.ac.nz>
2021-05-10 15:49:55 +1200

    Update configure

--- mapclientplugins/meshmergerstep/step.py
+++ mapclientplugins/meshmergerstep/step.py
@@ -103,7 +103,7 @@ class MeshMergerStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         """
-        dlg = ConfigureDialog()
+        dlg = ConfigureDialog(QtWidgets.QApplication.activeWindow().current_widget())
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
